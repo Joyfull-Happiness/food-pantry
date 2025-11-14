44c4643d81cbc093df1850ab0047f93e2a608926
rachelpilgrim26 <pilgrimrachel5@gmail.com>
2025-11-14 16:52:22 -0500

    fixed the code for my post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
 import Form from "./Components/Form";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Home from "./Pages/Home";
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
-
 export default function App() {
-
-  return (
-    <Home />
-  );
+  return <Home />;
 }
