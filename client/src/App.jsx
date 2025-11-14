9c48907b8d58c79a5f5c2f22e5ed96a0b47bb079
rachelpilgrim26 <pilgrimrachel5@gmail.com>
2025-11-14 14:18:18 -0500

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,14 @@ import React, { useState, useEffect } from "react";
 import Header from "/";
 import Form from "./Components/Form";
 
-
-function App() {
-
 export default function App() {
   return (
     <>
-     <div>
-      < Header navbarSpot={<Navbar/>}/>
-      <Footer />
-      <Form />
-    </div>
+      <div>
+        <Header navbarSpot={<Navbar />} />
+        <Footer />
+        <Form />
+      </div>
     </>
   );
-
 }
-
-
